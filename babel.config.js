992f9c04d58e9705f2af3f9d86ba4565d4479849
vinayakanarookutty <vinayakanarookutty>
2026-08-16 08:24:01 +0530

    updatyed

--- babel.config.js
+++ babel.config.js
@@ -4,9 +4,6 @@ module.exports = function (api) {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
-    ],
-    plugins: [
-      "react-native-reanimated/plugin" // <- add this (last)
-    ],
+    ]
   };
 };
